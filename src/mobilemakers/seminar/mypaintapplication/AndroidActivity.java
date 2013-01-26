6972d99f191eedec4b245680aaf9e199e1386864
Paul Szerlip <paul.szerlip@knights.ucf.edu>
2013-01-26 11:19:33 -0500

    Changed activity name

--- src/mobilemakers/seminar/mypaintapplication/AndroidActivity.java
+++ src/mobilemakers/seminar/mypaintapplication/AndroidActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class AndroidActivity extends Activity {
   
   @Override
   public void onCreate(Bundle savedInstanceState) {
