fb9c345be698b9718cc71a0b170a30f277ac598f
Paul Szerlip <paul.szerlip@knights.ucf.edu>
2013-02-06 00:20:45 -0500

    Simple5, paints blue circles at touch down events, we can draw!

--- src/mobilemakers/seminar/mypaintapplication/MySurfaceView.java
+++ src/mobilemakers/seminar/mypaintapplication/MySurfaceView.java
@@ -1,20 +1,45 @@
 package mobilemakers.seminar.mypaintapplication;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-public class MySurfaceView extends SurfaceView implements OnTouchListener {
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback, OnTouchListener {
 
+
+	private SurfaceHolder sholder;
+	private Bitmap bitmap;
+	private Paint p; // added
+	
     /**
      * Constructor for a surfaceview, pretty standard. Registers for touches, and creates our drawing thread. 
      * @param context
      */
 	public MySurfaceView(Context context) {
 		super(context);
+		
+		//SurfaceHolders are responsible for getting us our canvas to draw on!
+		SurfaceHolder s = this.getHolder();
+		
+		//get surface event callbacks, letting us know when our surface is ready for drawing
+		s.addCallback(this);
+		
+		//create our paint object
+		p = new Paint();
+
+		//set our color and width defaults, these can be adjusted later
+		p.setColor(Color.CYAN);
+		p.setStrokeWidth(5);
 		
 		//can you focus on this view object?
 		this.setFocusable(true);
@@ -36,6 +61,13 @@ public class MySurfaceView extends SurfaceView implements OnTouchListener {
 			//the user has just started pressing the screen
 			case MotionEvent.ACTION_DOWN:
 				Log.d("touch", "down");
+				
+				//print our point
+				updateBitmap(new PointF(e.getX(), e.getY()));
+				
+				//update our screen!
+				drawToScreen();
+				
 				break;		
 				
 				//our user has moved their finger, but yet to withdraw it from the screen
@@ -60,5 +92,59 @@ public class MySurfaceView extends SurfaceView implements OnTouchListener {
 		//we're done handling the event, let Android know
 		return true;
 	  }
+	  
+	  private void drawToScreen()
+	  {
+		  //get the canvas from our surface holder
+		  Canvas c = sholder.lockCanvas();
+		  
+		  //handle the drawing on our object
+		  c.drawBitmap(bitmap, 0, 0, p);	
+		
+		  //release the canvas object for painting to the screen
+		  sholder.unlockCanvasAndPost(c);
+		  
+	  }
+	  
+	  private void updateBitmap(PointF pointToDraw)
+	  {
+		  Canvas c = new Canvas(bitmap);
+		  
+		  //do our drawing of points here!
+		  c.drawCircle(pointToDraw.x, pointToDraw.y, 5, p);
+	  }
+	  
+	  private void setupBitmap()
+	  {
+		  Rect frame = sholder.getSurfaceFrame();
+		  int width = frame.width();
+		  int height = frame.height();
+		  bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+	  }
+	  
+		 /**
+		  * Pass surface information to the thread for storing and updating.
+		  */
+		public void surfaceChanged(SurfaceHolder s, int format, int width, int height) {
+			this.sholder = s;
+			setupBitmap();
+		}
+
+		/**
+		 * The surface has been created, we need to start our update process.
+		 */
+		public void surfaceCreated(SurfaceHolder s) {
+			this.sholder = s;
+			setupBitmap();
+		}
+
+		/**
+		 * Need to make sure we stop our thread/drawing when we destroy our surface. 
+		 */
+		public void surfaceDestroyed(SurfaceHolder s) {
+			this.sholder = null;
+		}
+	  
+	  
 	
 }
